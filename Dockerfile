f9a22e47eb5340d47d915bf35b83eacf3c0495b4
Victor Andrey <vandrey.souzat@gmail.com>
2026-09-12 14:22:25 -0400

    feat: use gunicorn for production

--- Dockerfile
+++ Dockerfile
@@ -8,4 +8,4 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 EXPOSE 5000
 
-CMD ["python", "api.py"]
+CMD ["gunicorn", "--bind", "0.0.0.0:5000", "api:app"]
--- api.py
+++ api.py
@@ -37,13 +37,8 @@ def metrics():
 
     return jsonify(resultado)
 
+
 @app.route("/dashboard")
 def dashboard():
 
     return render_template("dashboard.html")
-
-import os
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
