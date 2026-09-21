91567664c06bb450d32ef66dccfaa16b334ba2c4
Victor Andrey <vandrey.souzat@gmail.com>
2026-09-21 14:48:47 -0400

    fix: correctly initialize csrf on account recovery

--- api.py
+++ api.py
@@ -1593,6 +1593,9 @@ def admin_alterar_role(user_id):
 @admin_required
 def admin_recuperar_victor():
 
+    # Garante que o token CSRF exista na sessão
+    obter_token_csrf()
+
     user_id = 2
 
     if request.method == "POST":
